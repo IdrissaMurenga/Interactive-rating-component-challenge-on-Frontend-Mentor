0b0e5485ec95681067c878e9cba61c64bf7f5791
Murenga Idrissa <idrissam@afriticgroup.com>
2022-12-23 00:36:11 -0400

    made a solution for the interactive rating

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Interactive-rating-component-challenge-on-Frontend-Mentor
+
+### Screenshot
+
+![](/design/active-states.jpg)
+![](/design/desktop-design.jpg)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Javascript
+
+
+## Author
+
+- Website - [Afritic](https://www.Afritic.com)
+- Frontend Mentor - [@IdrissaMurenga](https://www.frontendmentor.io/profile/IdrissaMurenga)
---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const submitBtn = document.querySelector("#submitBtn");
+const num = document.querySelectorAll(".number")
+const span = document.querySelector("#span")
+const hero = document.querySelector(".hero")
+const innerContainer = document.querySelector(".inner_container")
+
+
+submitBtn.addEventListener("click",()=>{
+    hero.classList.remove("hero");
+    innerContainer.style.display = "none"
+})
+num.forEach((num)=>{
+    num.addEventListener("click",()=>{
+        num.style.backgroundColor = "#fb7413"
+        span.innerHTML = num.innerHTML
+    })
+})
+
